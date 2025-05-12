344dd6622524f449b690dcaa371aa5566c3a7c4b
Matheus Henrique <matheus.hsilvaa18@gmail.com>
2025-05-11 22:24:57 -0300

    create: software que analise extratos bancários para que o usuário possa ter uma visão melhor de suas finanças, utilizando o princípio KISS (Keep It Short and Simple).

---
+++ src/main/java/com/bank/transaction/analyzer/App.java
@@ -0,0 +1,35 @@
+package com.bank.transaction.analyzer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class App {
+    private static final String RESOURCES = "src/main/resources/";
+    public static void main(String[] args)  throws IOException  {
+       // Ler a entrada;
+        final Path path = Paths.get(RESOURCES + args[0]);
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        // Analisar a entrada em determinado formato;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        for(final String line: lines) {
+            final String[] columns = line.split(",");
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+
+            if (date.getMonth() == Month.JANUARY) {
+                // processar o resultado
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+        }
+
+        // Relatar um resumo do resultado.
+        System.out.println("The total for all transactions is " + total); 
+    }
+}
---
+++ src/test/java/com/bank/transaction/analyzer/AppTest.java
@@ -0,0 +1,19 @@
+package com.bank.transaction.analyzer;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
